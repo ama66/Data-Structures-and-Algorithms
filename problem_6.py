8c005bd81776287c433181844c715520b76a1d67
Ammar Abdilghanie <ama66@cornell.edu>
2019-07-30 17:41:28 -0500

    Add files via upload

---
+++ problem_6.py
@@ -0,0 +1,27 @@
+##
+def get_min_max(ints):
+    """
+    Return a tuple(min, max) out of list of unsorted integers.
+
+    Args:
+       ints(list): list of integers containing one or more integers
+    """
+    n=len(ints)
+    minn=float("inf")
+    maxn=-float("inf")
+    
+    for i in range(n):
+        if ints[i]> maxn:
+            maxn=ints[i]
+        if ints[i] < minn:
+            minn=ints[i]
+    return minn,maxn
+
+
+## Example Test Case of Ten Integers
+import random
+
+l = [i for i in range(0, 10)]  # a list containing 0 - 9
+random.shuffle(l)
+
+print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
