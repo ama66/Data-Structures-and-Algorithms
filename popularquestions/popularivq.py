78c1363006125b2a85153370e53937ed4dc8e099
Ammar Abdilghanie <ama66@cornell.edu>
2020-05-31 13:30:39 -0500

    Update popularivq.py

--- popularquestions/popularivq.py
+++ popularquestions/popularivq.py
@@ -157,3 +157,64 @@ print(b)
 print(merge2([a, b]))
 # 123456
 
+
+######## Bruteforce 
+
+from __future__ import print_function
+from heapq import *
+
+
+class ListNode:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+
+def merge_lists(lists):
+  ## Bruteforce
+    elems=[]
+
+    for l in lists:
+        resultHead = l
+        while resultHead:
+            elems.append(resultHead.value)
+            resultHead=resultHead.next
+    elems.sort()
+    
+    idx=0
+    head=ListNode(elems[idx])
+    cur=head
+    while idx < len(elems):
+        idx+=1 
+        cur.next=ListNode(idx)
+        cur=cur.next 
+    
+        
+    
+    # TODO: Write your code here
+    return head
+
+
+def main():
+  # l1 = ListNode(2)
+  # l1.next = ListNode(6)
+  # l1.next.next = ListNode(8)
+  l1=ListNode(2,ListNode(6,ListNode(8)))
+  # l2 = ListNode(3)
+  # l2.next = ListNode(6)
+  # l2.next.next = ListNode(7)
+  l2=ListNode(3,ListNode(6,ListNode(7)))
+  # l3 = ListNode(1)
+  # l3.next = ListNode(3)
+  # l3.next.next = ListNode(4)
+  l3=ListNode(1,ListNode(3,ListNode(4)))
+
+  result = merge_lists([l1, l2, l3])
+  print("Here are the elements form the merged list: ", end='')
+  while result != None:
+    print(str(result.value) + " ", end='')
+    result = result.next
+
+
+main()
+
