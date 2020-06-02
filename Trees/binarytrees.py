6c4503c79a1d7984dbeb2428a72d32f18b6ebbec
Ammar Abdilghanie <ama66@cornell.edu>
2020-06-02 01:07:19 -0500

    Update binarytrees.py

--- Trees/binarytrees.py
+++ Trees/binarytrees.py
@@ -1,7 +1,6 @@
 ## implement Binary Tree methods
 
 class Node:
-class Node:
     def __init__(self,info):
         self.info=info
         self.lchild=None
@@ -21,6 +20,32 @@ class BinaryTree:
         self._preorder(rootnode.lchild)
         self._preorder(rootnode.rchild)
         
+    def inorder(self):
+        self._inorder(self.root)
+        print()
+    
+    def _inorder(self,rootnode):
+        if rootnode==None:
+            return
+        self._inorder(rootnode.lchild)
+        print(rootnode.info, " ", end="")
+        self._inorder(rootnode.rchild)
+        
+    def postorder(self):
+        self._postorder(self.root)
+        print()
+    
+    def _postorder(self,rootnode):
+        if rootnode==None:
+            return
+        self._postorder(rootnode.lchild)
+        self._postorder(rootnode.rchild)
+        print(rootnode.info, " ", end="")
+        
+        
+        
+        
+        
     def create_tree(self):
         self.root=Node("P")
         self.root.lchild=Node("Q")
@@ -33,4 +58,14 @@ bt=BinaryTree()
 bt.create_tree()
 print("This is preorder traversal of the tree")
 bt.preorder()
+print("This is inorder traversal of the tree")
+bt.inorder()
+print("This is postorder traversal of the tree")
+bt.postorder()
 
+This is preorder traversal of the tree
+P  Q  A  B  R  X  
+This is inorder traversal of the tree
+A  Q  B  P  X  R  
+This is postorder traversal of the tree
+A  B  Q  X  R  P  
