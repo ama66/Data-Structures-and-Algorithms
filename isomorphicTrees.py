5cabace638eae05e3d9e81bcd349e45916431813
Ammar Abdilghanie <ama66@cornell.edu>
2020-06-22 02:01:39 -0500

    Update isomorphicTrees.py

--- isomorphicTrees.py
+++ isomorphicTrees.py
@@ -1,35 +1,27 @@
-################
-## Python program to check if two given trees are isomorphic 
-#############################  
-# A Binary tree node 
+## Check if two binary trees are isomorphic (structurally similar even though may take different forms)
+#################
+
 class Node: 
     def __init__(self, data): 
         self.data = data 
         self.left = None
         self.right = None
       
-# Check if the binary tree is isomorphic or not 
-def isIsomorphic(n1, n2): 
+def Is_Isomorphic(node1, node2): 
     
-    if n1 is None and n2 is None: 
+    if node1 is None and node2 is None: 
         return True
   
-    if n1 is None or n2 is None: 
+    if node1 is None or node2 is None: 
         return False
   
-    if n1.data != n2.data : 
+    if node1.data != node2.data : 
         return False
-   
-   # There are two possible cases for n1 and n2 to be isomorphic 
-    # Case 1: The subtrees rooted at these nodes have NOT 
-    # been "Flipped". 
-    # Both of these subtrees have to be isomorphic, hence the && 
-    # Case 2: The subtrees rooted at these nodes have 
-    # been "Flipped" 
-    return ((isIsomorphic(n1.left, n2.left)and 
-            isIsomorphic(n1.right, n2.right)) or
-            (isIsomorphic(n1.left, n2.right) and 
-            isIsomorphic(n1.right, n2.left)) 
+
+    return ((Is_Isomorphic(node1.left, node2.left)and 
+            Is_Isomorphic(node1.right, node2.right)) or
+            (Is_Isomorphic(node1.left, node2.right) and 
+            Is_Isomorphic(node1.right, node2.left)) 
             ) 
 ####      1
 ##      /   \ 
@@ -39,14 +31,14 @@ def isIsomorphic(n1, n2):
 #       / \ 
 #      7  8
 # Driver program to test above function 
-n1 = Node(1) 
-n1.left = Node(2) 
-n1.right = Node(3) 
-n1.left.left = Node(4) 
-n1.left.right = Node(5) 
-n1.right.left = Node(6) 
-n1.left.right.left = Node(7) 
-n1.left.right.right = Node(8) 
+Node1 = Node(1) 
+Node1.left = Node(2) 
+Node1.right = Node(3) 
+Node1.left.left = Node(4) 
+Node1.left.right = Node(5) 
+Node1.right.left = Node(6) 
+Node1.left.right.left = Node(7) 
+Node1.left.right.right = Node(8) 
 
 ####       1
 ##      /     \ 
@@ -56,17 +48,16 @@ n1.left.right.right = Node(8)
 ##              /  \ 
 ##             8    7
 
-n2 = Node(1) 
-n2.left = Node(3) 
-n2.right = Node(2) 
-n2.right.left = Node(4) 
-n2.right.right = Node(5) 
-n2.left.right = Node(6) 
-n2.right.right.left = Node(8) 
-n2.right.right.right  = Node(7) 
+Node2 = Node(1) 
+Node2.left = Node(3) 
+Node2.right = Node(2) 
+Node2.right.left = Node(4) 
+Node2.right.right = Node(5) 
+Node2.left.right = Node(6) 
+Node2.right.right.left = Node(8) 
+Node2.right.right.right  = Node(7) 
   
-print("Yes" if (isIsomorphic(n1, n2) == True) else "No")
+Is_Isomorphic(Node1, Node2) 
   
-# This code is contributed by Nikhil Kumar Singh(nickzuck_007) 
+True
 
-Yes
